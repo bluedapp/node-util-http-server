9423ef3ecc86a027d5910a9576ebeb86405686eb
yaf <lov.2014@gmail.com>
2020-07-15 17:25:07 +0800

    chore: update version

--- packages/http-server-base/test/unit/index.spec.ts
+++ packages/http-server-base/test/unit/index.spec.ts
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { expect } from 'chai'
-import main from '../../src/index'
+import { createServer } from '../../src/index'
 
 describe('normal', () => {
-  it(`function return string`, done => {
-    const res = main()
-    expect(res).to.be.a('string')
+  it(`function`, done => {
+    expect(createServer).to.be.a('function')
     done()
   })
 })
